1acab45da135ffcf304b76b3e21651f26a07c686
manucho254 <manuchoadero@gmail.com>
2021-12-14 17:51:34 +0300

    login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,7 +2,9 @@
 <div class="login-page">
     <div class="columns p-3 mt-5">
         <div class="column is-4 is-offset-4">
-            <h1 class="title">Log in</h1>
+          <div class="box is-light mt-4">
+            <h1 class="title has-text-centered is-family-monospace">LogIn</h1>
+            <hr>
 
             <form @submit.prevent="submitForm">
                 <div class="field">
@@ -20,12 +22,13 @@
                 <div class="notification is-danger" v-if="errors.length">
                     <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
                 </div>
-                <div class="field">
+                <div class="field mt-3">
                     <div class="control">
-                        <button class="button is-dark">log in</button>
+                        <button class="button is-dark">Log In</button>
                     </div>
                 </div>
             </form>
+          </div>
         </div>
     </div>
 </div>
